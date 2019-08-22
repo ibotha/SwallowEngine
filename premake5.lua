25e29c75d6403766621da451644b1831cf86044f
Isard BOTHA <ibotha@c4r24s4.wethinkcode.co.za>
2019-08-22 14:46:10 +0200

    Moved project structure

--- premake5.lua
+++ premake5.lua
@@ -1,110 +1,9 @@
-workspace "Swallow"
+workspace "Sandbox"
 
-	startproject "Sandbox"
-	architecture "x64"
+startproject "Sandbox"
 
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
+include "Swallow"
 
-	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-	-- Include directoies relative to root folder (solution directory)
-	IncludeDir = {}
-	IncludeDir["GLFW"] = "Swallow/vendor/GLFW/include"
-	IncludeDir["Glad"] = "Swallow/vendor/Glad/include"
-	IncludeDir["ImGui"] = "Swallow/vendor/imgui"
-	IncludeDir["glm"] = "Swallow/vendor/glm/glm"
-	IncludeDir["stb_image"] = "Swallow/vendor/stb_image"
-
-	group "Dependencies"
-		include "Swallow/vendor/GLFW"
-		include "Swallow/vendor/Glad"
-		include "Swallow/vendor/imgui"
-	group ""
-
-project "Swallow"
-	location "Swallow"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "swpch.hpp"
-	pchsource "Swallow/src/swpch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.hpp",
-		"%{prj.name}/src/**.hpp",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl",
-		"%{prj.name}/vendor/stb_image/**",
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.stb_image}"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"opengl32.lib"
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
-
-	filter "system:macosx"
-		systemversion "latest"
-
-		defines
-		{
-			"SW_PLATFORM_MACOSX",
-			"GLFW_INCLUDE_NONE"
-		}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"SW_PLATFORM_WINDOWS",
-			"GLFW_INCLUDE_NONE"
-		}
-
-	filter "configurations:Debug"
-		defines "SW_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "SW_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "SW_DIST"
-		runtime "Release"
-		optimize "on"
 
 project "Sandbox"
 	location "Sandbox"
@@ -126,10 +25,10 @@ project "Sandbox"
 	{
 		"Swallow/vendor/spdlog/include",
 		"Swallow/src",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.ImGui}"
+		"Swallow/%{IncludeDir.GLFW}",
+		"Swallow/%{IncludeDir.Glad}",
+		"Swallow/%{IncludeDir.glm}",
+		"Swallow/%{IncludeDir.ImGui}"
 	}
 
 	links
