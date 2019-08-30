52e57add3d5da8f3d50d9ea6a0f215443d450413
Pieter PREEZ <ppreez@c4r12s9.wethinkcode.co.za>
2019-08-30 14:26:30 +0200

    Added texture material

--- Swallow/src/Swallow/Renderer/texture/TextureMaterialInstance.cpp
+++ Swallow/src/Swallow/Renderer/texture/TextureMaterialInstance.cpp
@@ -6,7 +6,7 @@
 /*   By: ppreez <ppreez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 16:12:58 by ppreez            #+#    #+#             */
-/*   Updated: 2019/08/28 16:59:33 by ppreez           ###   ########.fr       */
+/*   Updated: 2019/08/30 14:23:16 by ppreez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,59 @@ namespace Swallow {
         }
         return *this;
     }
+
+    // Methods
+
+    void TextureMaterialInstance::Bind()
+    {
+        m_Texture->Bind();
+    }
+
+    void TextureMaterialInstance::Unbind()
+    {
+        m_Texture->Unbind();
+    }
+
+    // Getters and setters
+
+    void TextureMaterialInstance::SetLightDirection(glm::vec3 LightDirection)
+    {
+        m_LightDirection = LightDirection;
+    }
+
+    void TextureMaterialInstance::SetViewProjection(glm::mat4 ViewProjection)
+    {
+        m_ViewProjection = ViewProjection;
+    }
+
+    void TextureMaterialInstance::SetRot(glm::mat4 Rot)
+    {
+        m_Rot = Rot;
+    }
+
+    void TextureMaterialInstance::SetModel(glm::mat4 Model)
+    {
+        m_Model = Model;
+    }
+
+    glm::vec3 TextureMaterialInstance::GetLightDirection()
+    {
+        return m_LightDirection;
+    }
+
+    glm::mat4 TextureMaterialInstance::GetViewProjection()
+    {
+        return m_ViewProjection;
+    }
+
+    glm::mat4 TextureMaterialInstance::GetRot()
+    {
+        return m_Rot;
+    }
+
+    glm::mat4 TextureMaterialInstance::GetModel()
+    {
+        return m_Model;
+    }
+
 }
--- Swallow/src/Swallow/Renderer/texture/TextureMaterialInstance.hpp
+++ Swallow/src/Swallow/Renderer/texture/TextureMaterialInstance.hpp
@@ -6,7 +6,7 @@
 /*   By: ppreez <ppreez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/28 16:09:54 by ppreez            #+#    #+#             */
-/*   Updated: 2019/08/28 16:56:44 by ppreez           ###   ########.fr       */
+/*   Updated: 2019/08/30 14:23:46 by ppreez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,25 @@ namespace Swallow {
         public:
             TextureMaterialInstance(Ref<Shader> shader);
             TextureMaterialInstance(Ref<Shader> shader, Ref<Texture2D> texture);
+            TextureMaterialInstance(TextureMaterialInstance const &rhs);
             TextureMaterialInstance &operator=(TextureMaterialInstance const &rhs);
             ~TextureMaterialInstance();
 
             void Bind();
             void Unbind();
+            void SetLightDirection(glm::vec3 LightDirection);
+            void SetViewProjection(glm::mat4 ViewProjection);
+            void SetRot(glm::mat4 Rot);
+            void SetModel(glm::mat4 Model);
+            glm::vec3 GetLightDirection();
+            glm::mat4 GetViewProjection();
+            glm::mat4 GetRot();
+            glm::mat4 GetModel();
         private:
             TextureMaterialInstance();
+            glm::vec3 m_LightDirection;
+            glm::mat4 m_ViewProjection;
+            glm::mat4 m_Rot;
+            glm::mat4 m_Model;
     };
 }
