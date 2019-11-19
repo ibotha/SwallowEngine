507eee1f848ea6da50c8ebf5cd25c1f066f8314e
Isard BOTHA <ibotha@c4r20s4.wethinkcode.co.za>
2019-11-19 17:30:11 +0200

    Okeh

--- Swallow/src/Swallow/Renderer/material/CharMaterial.cpp
+++ Swallow/src/Swallow/Renderer/material/CharMaterial.cpp
@@ -44,10 +44,10 @@ namespace Swallow
                 color = textColor * sampled;
             }
 		)";
-        m_Shader.reset();
+        // m_Shader.reset();
 		m_Shader = Shader::Create(sVertexSrc, sFragmentSrc);
 
-        m_Chars->clear();
+        // m_Chars->clear();
         FT_Library ft;
         FT_Face face;
         int error = FT_Init_FreeType(&ft);
